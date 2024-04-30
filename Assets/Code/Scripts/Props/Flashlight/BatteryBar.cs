e84d15d9dca557ae2ddd5fc059eebdf13f4bfebf
Mohammed alzowaid <102298394+Medo-02@users.noreply.github.com>
2024-04-30 19:54:46 +0300

    Battery Bar

---
+++ Assets/Code/Scripts/Props/Flashlight/BatteryBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryBar : MonoBehaviour
+{
+    private Image BatteryBarIM;
+    public float CurrentBattery;
+    private float MaxBattery = 100f;
+    FlashlightAdvanced flashlight;
+
+
+
+
+
+    void Start()
+    {
+        BatteryBarIM = GetComponent<Image>();
+        flashlight = FindObjectOfType<FlashlightAdvanced>();
+
+    }
+
+
+
+    void Update()
+    {
+        CurrentBattery = flashlight.lifetime;
+        BatteryBarIM.fillAmount = CurrentBattery / MaxBattery;
+
+    }
+}
