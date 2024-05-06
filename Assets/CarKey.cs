aa1569f7cc2b3471efc02afc0d663cecc54dcc9f
Mohammed alzowaid <102298394+Medo-02@users.noreply.github.com>
2024-05-06 21:54:31 +0300

    car key emit light when pressed

---
+++ Assets/CarKey.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class CarKey : MonoBehaviour
+{
+    public GameObject CarKeys;
+    private Light indicatorLight;
+    private Renderer indicatorRenderer;
+    private Material originalMaterial;
+    public Material redMaterial;
+
+    void Start()
+    {
+        Transform Indicator = CarKeys.transform.Find("Indicator");
+        if (Indicator != null)
+        {
+            indicatorRenderer = Indicator.GetComponent<Renderer>();
+            if (indicatorRenderer != null)
+            {
+                originalMaterial = indicatorRenderer.material;
+            }
+            else
+            {
+                Debug.LogWarning("Indicator Renderer not found!");
+            }
+            indicatorLight = Indicator.GetComponent<Light>();
+        }
+        else
+        {
+            Debug.LogWarning("Indicator not found!");
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("flashlight"))
+        {
+            if (indicatorRenderer != null && indicatorLight != null)
+            {
+                StartCoroutine(FlashIndicator());
+            }
+        }
+    }
+
+    IEnumerator FlashIndicator()
+    {
+        indicatorLight.enabled = true;
+
+        // Change material color to red
+        indicatorRenderer.material = redMaterial;
+
+        // Wait for 1 second
+        yield return new WaitForSeconds(1.0f);
+
+        // Revert material color back to original
+        indicatorRenderer.material = originalMaterial;
+
+        // Turn off the light
+        indicatorLight.enabled = false;
+    }
+}
