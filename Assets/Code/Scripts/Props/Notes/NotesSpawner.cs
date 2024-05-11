8f016f6172e3b79870dde59762b76cb4ccc59672
Mohammed alzowaid <moha-a-36@hotmail.com>
2024-05-11 16:54:42 +0300

    Add notes minimap

--- Assets/Code/Scripts/Props/Notes/NotesSpawner.cs
+++ Assets/Code/Scripts/Props/Notes/NotesSpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class NotesSpawner : MonoBehaviour
 {
     public GameObject[] notes;
+    public GameObject objectToSpawn; // The GameObject you want to spawn
     public GameObject SpawnOB;
     private Transform[] spawnPoints;
 
@@ -10,6 +11,7 @@ public class NotesSpawner : MonoBehaviour
     {
         FindSpawnPoints();
         SpawnAllNotes();
+        objectToSpawn.SetActive(false);
     }
 
     void SpawnAllNotes()
@@ -17,8 +19,21 @@ public class NotesSpawner : MonoBehaviour
         for (int i = 0; i < notes.Length; i++)
         {
             int spawnIndex = Random.Range(0, spawnPoints.Length);
-            notes[i].transform.position = spawnPoints[spawnIndex].position;
-            notes[i].transform.rotation = spawnPoints[spawnIndex].rotation;
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            // Set the position and rotation of the note itself
+            notes[i].transform.position = spawnPoint.position;
+            notes[i].transform.rotation = spawnPoint.rotation;
+            // Spawn the new object at the same position as the note
+            GameObject spawnedObject = Instantiate(objectToSpawn, notes[i].transform.position, Quaternion.identity);
+
+            // Set the rotation of the spawned object to face the sky
+            Vector3 upDirection = Vector3.up;
+            Vector3 toSky = -Vector3.up;
+            Quaternion rotation = Quaternion.LookRotation(toSky, upDirection);
+            spawnedObject.transform.rotation = rotation;
+
+            // Set the note as the parent of the spawned object
+            spawnedObject.transform.parent = notes[i].transform;
             notes[i].SetActive(true);
         }
     }
