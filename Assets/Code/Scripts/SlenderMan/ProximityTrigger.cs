7109e91d5f5989ca883fd629077fbaef7cca68b3
mohammedAlmaghlout <mo.maghlouth@gmail.com>
2024-05-11 14:19:33 +0300

    add slender man ai

---
+++ Assets/Code/Scripts/SlenderMan/ProximityTrigger.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ProximityTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public VideoPlayer videoPlayer;
+    public float proximityDistance = 15f;
+    private float timeClose = 0f;
+
+    private void Update()
+    {
+        if (!player.activeInHierarchy || !gameObject.activeInHierarchy)
+        {
+            
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+       
+
+        if (distance <= proximityDistance)
+        {
+            timeClose += Time.deltaTime;
+           
+
+            if (timeClose >= 5f)
+            {
+               
+                videoPlayer.Play();
+                StartCoroutine(WaitAndRestart((float)videoPlayer.length));
+            }
+        }
+        else
+        {
+            timeClose = 0f; 
+            
+        }
+    }
+
+    private IEnumerator WaitAndRestart(float delay)
+    {
+       
+        yield return new WaitForSeconds(delay);
+       
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
